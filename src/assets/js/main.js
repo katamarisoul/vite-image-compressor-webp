51b0bd5591f3f47fb72135011d2a201701ad6b3d
hikaru matsuura <matsuura@juxtaposition.co.jp>
2024-11-19 02:02:42 +0900

    add：viewportFixed.jsを追加
    デバイス幅375px以下の場合、viewportを375pxに固定する。

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -5,8 +5,10 @@
 // modulepreloadのポリフィル
 // これを記述していないと、以下の記述のみのjsファイルがビルドされてしまう
 import 'vite/modulepreload-polyfill';
+import { viewportFixed } from './modules/viewportFixed';
 import { SmoothScroll } from './modules/SmoothScroll';
 
 document.addEventListener('DOMContentLoaded', () => {
+  viewportFixed();
   new SmoothScroll();
 });
---
+++ src/assets/js/modules/viewportFixed.js
@@ -0,0 +1,37 @@
+// ------------------------------
+// ES Modules / viewportFixed
+// ------------------------------
+
+/**
+ * デバイス幅が375px未満の場合、viewportを固定
+ * @function
+ * @see https://zenn.dev/tak_dcxi/articles/690caf6e9c4e26
+ */
+export function viewportFixed() {
+  !(function () {
+    /**
+     * viewportを設定するmeta要素を取得
+     * @type {HTMLElement}
+     */
+    const viewport = document.querySelector('meta[name="viewport"]');
+
+    /**
+     * ビューポートを切り替える関数
+     * @function
+     */
+    const switchViewport = () => {
+      /**
+       * ビューポートの設定値
+       * @type {string}
+       */
+      const value = window.outerWidth > 375 ? 'width=device-width,initial-scale=1' : 'width=375';
+
+      if (viewport.getAttribute('content') !== value) {
+        viewport.setAttribute('content', value);
+      }
+    };
+
+    addEventListener('resize', switchViewport, false);
+    switchViewport();
+  })();
+}
